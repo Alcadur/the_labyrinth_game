772136b34fad364ad8f7dec1d6242fe0b3072fca
Piotr Chełmicki <chelmicki.p@gmail.com>
2021-04-08 17:17:50 +0200

    create mapper for between tile map ids and directions

--- src/sceans/world/world.gd
+++ src/sceans/world/world.gd
@@ -2,16 +2,43 @@ extends Node2D
 
 onready var tiles := $TileMap
 var dirs = {
-	"W": 0x1,
-	"N": 0x2,
-	"E": 0x4,
-	"S": 0x8
+	"N": 0x1,
+	"E": 0x2,
+	"S": 0x4,
+	"W": 0x8
 }
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var map = $worldTileMap
 	#var c = $TileMap.get_cellv(Vector2(0,0))
-	$TileMap.set_cellv(Vector2(0,0), -1)
+	#$TileMap.set_cellv(Vector2(0,0), -1)
+	print( [dirs.E, dirs.N,  dirs.E|dirs.N])
+	#WorldTileMapHelper.set_cell_value(map, Vector2(2, 2), dirs.N|dirs.E|dirs.S|dirs.W)
+	WorldTileMapHelper.set_cell_value(map, Vector2(1, 1), dirs.N)
+	WorldTileMapHelper.set_cell_value(map, Vector2(3, 1), dirs.E)
+	WorldTileMapHelper.set_cell_value(map, Vector2(5, 1), dirs.S)
+	WorldTileMapHelper.set_cell_value(map, Vector2(7, 1), dirs.W)
+	
+	WorldTileMapHelper.set_cell_value(map, Vector2(9, 1), dirs.N|dirs.E)
+	WorldTileMapHelper.set_cell_value(map, Vector2(11, 1), dirs.N|dirs.S)
+	WorldTileMapHelper.set_cell_value(map, Vector2(13, 1), dirs.N|dirs.W)
+	
+	WorldTileMapHelper.set_cell_value(map, Vector2(15, 1), dirs.E|dirs.S)
+	WorldTileMapHelper.set_cell_value(map, Vector2(17, 1), dirs.E|dirs.W)
+	
+	WorldTileMapHelper.set_cell_value(map, Vector2(19, 1), dirs.S|dirs.W)
+	
+	WorldTileMapHelper.set_cell_value(map, Vector2(1, 3), dirs.N|dirs.E|dirs.S)
+	WorldTileMapHelper.set_cell_value(map, Vector2(3, 3), dirs.N|dirs.E|dirs.W)
+	WorldTileMapHelper.set_cell_value(map, Vector2(5, 3), dirs.N|dirs.S|dirs.W)
+	
+	WorldTileMapHelper.set_cell_value(map, Vector2(7, 3), dirs.E|dirs.S|dirs.W)
+	
+	WorldTileMapHelper.set_cell_value(map, Vector2(9, 3), dirs.N|dirs.E|dirs.S|dirs.W)
+	
+	WorldTileMapHelper.set_cell_value(map, Vector2(1, 5), dirs.N|dirs.E|dirs.S|dirs.W)
+	
 	pass # Replace with function body.
 
 
---
+++ src/sceans/world/world_tile_map_helper.gd
@@ -0,0 +1,26 @@
+class_name WorldTileMapHelper 
+
+const _map = {
+	0: 0, # empty
+	1: 1, # N
+	2: 2, # E
+	3: 5, # N + E
+	4: 3, # S
+	5: 6, # N + S
+	6: 8, # E + S
+	7: 13, # N + E + S
+	8: 4, # W
+	9: 7, # W + N
+	10: 9, # E + W
+	11: 12, # N + E + W
+	12: 10, # S + W
+	13: 11, # N + S + W 
+	14: 14, # E + S + W
+	15: 15, # N + E + S + W
+}
+
+static func get_tile_id(field_value: int) -> int:
+	return _map[field_value]
+
+static func set_cell_value(map: TileMap, position: Vector2, value) -> void:
+	map.set_cellv(position, get_tile_id(value))
